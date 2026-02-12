e7051bc1f108051105ee852a08a2ecf7b0d296e9
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-02-12 19:43:01 -0300

    update: inserindo um CRUD básico e testando com o Insomnia(collections adicionadas)

--- PrimeiraAPI/index.js
+++ PrimeiraAPI/index.js
@@ -2,13 +2,48 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
+// Criando um CRUD(Create, Read, Update e Delete)
+
 const cursos = ["Node JS", "JavaScript", "React Native", "Java", "Scrum"];
 
-// localhost:3000/curso/:id
-server.get("/curso/:index", (req, res) => {
+// Listando Todos os Cursos
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+
+// Listando um Curso com base no index
+server.get("/cursos/:index", (req, res) => {
   const { index } = req.params;
 
   return res.json(cursos[index]);
+});
+
+// Criando um Novo Curso
+server.post("/cursos", (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+//Atualizando um Curso
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+});
+
+// Deletando um Curso
+server.delete("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
+  return res.json({ message: "Curso deletado com sucesso!" });
 });
 
 server.listen(3000);
